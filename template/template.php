5bd138a4ccd899ed6fc0c5e48e33f8dfb813564f
sw092180 <stephen.kolar@hpe.com>
2017-05-03 11:30:58 -0400

    go to blue

--- template/template.php
+++ template/template.php
@@ -5,7 +5,7 @@
     <title><?php pageTitle(); ?> | <?php siteName(); ?></title>
     <style type="text/css">
         .wrap { max-width: 720px; margin: 50px auto; padding: 30px 40px; text-align: center; box-shadow: 0 4px 25px -4px #9da5ab; }
-        article { text-align: left; padding: 40px; line-height: 150%; background-color: green; }
+        article { text-align: left; padding: 40px; line-height: 150%; background-color: blue; }
     </style>
 </head>
 <body>
