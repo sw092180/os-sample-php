500fa297c8d4ad0773235d3361530f5132c6682b
sw092180 <stephen.kolar@hpe.com>
2017-05-03 11:33:28 -0400

    change to blue try #2

--- template/template.php
+++ template/template.php
@@ -5,7 +5,7 @@
     <title><?php pageTitle(); ?> | <?php siteName(); ?></title>
     <style type="text/css">
         .wrap { max-width: 720px; margin: 50px auto; padding: 30px 40px; text-align: center; box-shadow: 0 4px 25px -4px #9da5ab; }
-        article { text-align: left; padding: 40px; line-height: 150%; background-color: blue; }
+        article { text-align: left; padding: 40px; line-height: 150%; background-color: #87CEEB; }
     </style>
 </head>
 <body>
